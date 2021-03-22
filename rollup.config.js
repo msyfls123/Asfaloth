2d11ced14656c1b4b024ad4d3481c171ac4a03de
msyfls123 <msyfls123@gmail.com>
2021-03-23 00:32:56 +0800

    make it works...

--- rollup.config.js
+++ rollup.config.js
@@ -3,15 +3,21 @@ import { nodeResolve } from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
 import svelte from 'rollup-plugin-svelte'
-import sveltePreprocess, { typescript as sveltePreprocessTypescript, scss as sveltePreprocessScss } from 'svelte-preprocess'
+import sveltePreprocess from 'svelte-preprocess'
 import svelteNodeGUIPreprocessor from '@nodegui/svelte-nodegui-preprocessor'
 import url from '@rollup/plugin-url'
+import replace from '@rollup/plugin-replace'
 
 const projectDir = __dirname
 const outDir = path.join(projectDir, 'dist')
 const srcDir = path.join(projectDir, 'src')
 
 const isDev = process.env.NODE_ENV !== 'production'
+
+const preprocessor = {
+    ...sveltePreprocess(),
+    ...svelteNodeGUIPreprocessor(),
+};
 
 const config = {
     plugins: [
@@ -28,23 +34,24 @@ const config = {
         url({
             include: ['**/*.svg', '**/*.png', '**/*.jp(e)?g', '**/*.gif', '**/*.webp', '**/*.node'],
         }),
+        replace({
+            '__DEV__': isDev,
+            '__TEST__': false,
+        }),
         svelte({
-            dev: isDev,
-            preprocess: {
-                ...svelteNodeGUIPreprocessor(),
-                ...sveltePreprocess(),
-            },
+            emitCss: false,
+            preprocess: preprocessor,
         }),
     ],
     input: path.join(srcDir, 'app.ts'),
     output: {
         format: 'cjs',
         dir: outDir,
-        sourcemap: isDev ? 'inline' : false,
+        sourcemap: isDev,
         compact: !isDev,
     },
     external: [
-        // '@nodegui/svelte-nodegui',
+        '@nodegui/nodegui',
     ]
 }
 
